37e9e5274f9ae30580c83612e572412049c28585
Bitmap Dev <dev@bitmap.land>
2024-01-16 14:17:05 +0000

    Remove block size from coord/index utils

--- examples/src/utils.ts
+++ examples/src/utils.ts
@@ -1,16 +1,13 @@
 import {
-  BLOCK_SIZE,
   BLOCKS_PER_COLUMN,
   BLOCKS_PER_EPOCH,
   BLOCKS_PER_ROW,
 } from './constants';
 
 export const getIndexFromCoords = (x: number, y: number) => {
-  const xNoBlockSize = Math.floor(x / BLOCK_SIZE);
-  const yNoBlockSize = Math.floor(y / BLOCK_SIZE);
-  const epochIndex = Math.floor(xNoBlockSize / BLOCKS_PER_ROW);
-  const column = xNoBlockSize % BLOCKS_PER_ROW;
-  const row = yNoBlockSize % BLOCKS_PER_COLUMN;
+  const epochIndex = Math.floor(x / BLOCKS_PER_ROW);
+  const column = x % BLOCKS_PER_ROW;
+  const row = y % BLOCKS_PER_COLUMN;
 
   return epochIndex * BLOCKS_PER_EPOCH + row * BLOCKS_PER_ROW + column;
 };
@@ -21,7 +18,7 @@ export const getCoordsFromIndex = (index: number) => {
   const column = index % BLOCKS_PER_ROW;
 
   return {
-    x: epochIndex * BLOCKS_PER_ROW * BLOCK_SIZE + column * BLOCK_SIZE,
-    y: row * BLOCK_SIZE,
+    x: epochIndex * BLOCKS_PER_ROW + column,
+    y: row,
   };
 };
